cef877a2d8a89163a098c776255fbe8e63b484c3
vanek021 <www.vanek01@mail.ru>
2023-03-01 23:08:51 +0500

    * Наброски верстки главной страницы

--- MoxControl/Controllers/HomeController.cs
+++ MoxControl/Controllers/HomeController.cs
@@ -6,7 +6,6 @@ using System.Diagnostics;
 
 namespace MoxControl.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;

ed7ee8e5eca4f5d937fea8e353aee45c6926f820
vanek021 <vanekmenyakin@gmail.com>
2023-03-11 13:04:34 +0500

    *Новые разделы

---
+++ MoxControl/Controllers/ServerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoxControl.Controllers
+{
+    public class ServerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

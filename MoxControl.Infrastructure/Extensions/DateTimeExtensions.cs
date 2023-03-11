0f576cded12510409330fd8a2beba72db3fab38d
vanek021 <vanekmenyakin@gmail.com>
2023-03-11 09:34:04 +0500

    * Frontend улучшения

---
+++ MoxControl.Infrastructure/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoxControl.Infrastructure.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        private static TimeSpan offset { get; set; } = TimeSpan.FromHours(3);
+
+        public static DateTime ToMoscowTime(this DateTime datetime)
+        {
+            var newDt = datetime + offset;
+            return newDt;
+        }
+
+        public static DateTime ToUTCFromMoscowTime(this DateTime datetime)
+        {
+            var newDt = datetime - offset;
+            return DateTime.SpecifyKind(newDt, DateTimeKind.Utc);
+        }
+    }
+}
---
+++ MoxControl/Extensions/ViewHelpExtensions.cs
@@ -0,0 +1,25 @@
+﻿using MoxControl.Infrastructure.Extensions;
+
+namespace MoxControl.Extensions
+{
+    public static class ViewHelpExtensions
+    {
+        public static string GetWelcomeMessage(this string username)
+        {
+            var moscowTime = DateTime.UtcNow.ToMoscowTime();
+
+            if (moscowTime.Hour <= 11 && moscowTime.Hour >= 5)
+            {
+                return $"Доброе утро, {username}!";
+            }
+
+            if (moscowTime.Hour >= 12 && moscowTime.Hour <= 16)
+                return $"Добрый день, {username}!";
+
+            if (moscowTime.Hour >= 17 && moscowTime.Hour <= 23)
+                return $"Добрый вечер, {username}!";
+
+            return $"Доброй ночи, {username}!";
+        }
+    }
+}

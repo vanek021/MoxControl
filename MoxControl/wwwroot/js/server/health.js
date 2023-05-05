28f8e49fc1a294c72e9880d8bbf0fb3156fc4f44
vanek021 <vanekmenyakin@gmail.com>
2023-05-05 08:08:42 +0500

    + Показатели на странице серверов

---
+++ MoxControl/wwwroot/js/server/health.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(async function () {
+    $("tr[data-server-row]").each(async function () {
+        var health = await getServerHealth($(this).attr("data-virtualizationSystem"), $(this).attr("data-server-id"));
+
+        var cpuProgress = $(this).find("div[data-server-cpu]");
+        var hddProgress = $(this).find("div[data-server-hdd]");
+        var ramProgress = $(this).find("div[data-server-ram]");
+
+        cpuProgress.find("div.progress-bar").attr("style", `width: ${health.cpuUsedPercent}%;`);
+        hddProgress.find("div.progress-bar").attr("style", `width: ${health.hddUsedPercent}%;`);
+        ramProgress.find("div.progress-bar").attr("style", `width: ${health.memoryUsedPercent}%;`);
+
+        cpuProgress.find("span").text(`CPU: ${health.cpuUsedPercent}%`);
+        hddProgress.find("span").text(`HDD: ${health.hddUsedPercent}%`);
+        ramProgress.find("span").text(`RAM: ${health.memoryUsedPercent}%`);
+    });
+});
+
+async function getServerHealth(virtualizationSystem, serverId) {
+    const response = await fetch(`/Server/GetServerHealth/${serverId}?virtualizationSystem=${virtualizationSystem}`);
+
+    if (response.ok) {
+        const jsonData = await response.json();
+        jsonData.isSuccess = true;
+        return jsonData;
+    }
+
+    var emptyData = {
+        cpuUsedPercent: 0,
+        memoryUsedPercent: 0,
+        hddUsedPercent: 0,
+        isSuccess: false
+    }
+
+    return emptyData;
+}
